d75e4021271f67618442fa5f3af87aa269a2769c
Josep Subils <josep@spellbook.io>
2017-06-07 11:50:42 +0200

    [update] identation update

--- base.js
+++ base.js
@@ -2,15 +2,15 @@ var s = require('spellbook')
 var chk = require('./chk.js')
 
 module.exports = {
-	status : {},
-	refresh : function (servidores, callback) {
-		s.epl(servidores, 1, (row, index, next) => {
-			chk(row, (data) => {
-				this.status[row] = data
-				next()
-			})
-		}, () => {
-			callback(this.status)
-		})
-	}
+  status : {},
+  refresh : function (servidores, callback) {
+    s.epl(servidores, 1, (row, index, next) => {
+      chk(row, (data) => {
+        this.status[row] = data
+        next()
+      })
+    }, () => {
+      callback(this.status)
+    })
+  }
 }
--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 var maquines = ['v1','v2']
-var express = require('express');
+var express = require('express')
 var b = require('./base.js')
-var app = express();
-var serveStatic = require('serve-static');
+var app = express()
+var serveStatic = require('serve-static')
 app.use(serveStatic('web', {'index': ['index.html', 'index.htm']}))
-app.listen(3000);
+app.listen(3000)
 
 var io = require('socket.io')({
-	transports: ['websocket'],
-});
+  transports: ['websocket'],
+})
 
-io.attach(4568);
+io.attach(4568)
 
 io.on('connection', function(socket){
-	socket.on('stats', function () {
-		b.refresh(maquines, (data) => {
-			socket.emit('stats', data)
-		})
-	})
+  socket.on('stats', function () {
+    b.refresh(maquines, (data) => {
+      socket.emit('stats', data)
+    })
+  })
 })
