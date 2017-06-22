8ab548ceb8a7f73cd94b3a28156cb40514f5e734
Josep Subils <josep@spellbook.io>
2017-06-22 14:31:26 +0200

    add disk usage

--- monode.js
+++ monode.js
@@ -1,20 +1,25 @@
 const conf = require('./conf.js')
 const os = require('os')
+const disk = require('diskusage')
 const socket = require('socket.io-client')(conf.server + ":" + conf.socket_port)
 console.log(`Conecting: ${conf.server + ":" + conf.socket_port}`)
 var stats
 socket.on('connect', () => {
   stats = setInterval(() => {
-    socket.emit('stats', {
-      key : conf.key,
-      hostname : os.hostname(),
-      freemem : os.freemem(),
-      totalmem : os.totalmem(),
-      loadavg : os.loadavg(),
-      network : os.networkInterfaces(),
-      platform : os.platform(),
-      relase : os.release(),
-      uptime : os.uptime()
+    disk.check("/", (err, info) => {
+      if (err) console.error(err)
+      socket.emit('stats', {
+        key : conf.key,
+        hostname : os.hostname(),
+        freemem : os.freemem(),
+        totalmem : os.totalmem(),
+        loadavg : os.loadavg(),
+        network : os.networkInterfaces(),
+        platform : os.platform(),
+        relase : os.release(),
+        uptime : os.uptime(),
+        diskusage : info
+      })
     })
   }, 2500)
 })
